68da5245774c72dac37bee62bbca55ac90f09e06
armorik83 <okunokentaro@me.com>
2015-03-05 21:59:30 +0900

    init

---
+++ README.md
@@ -0,0 +1,65 @@
+#cw-log
+
+[![Build Status][travis-image]][travis-url]
+[![NPM version][npm-image]][npm-url]
+[![License][license-image]][license-url]
+
+`cw-log` is a simple and level controllable logger.
+
+```
+$npm install cw-log --save
+```
+
+## Example
+
+For node.
+
+```
+var log = require('cw-log').logger(6);
+```
+
+For a browser.
+
+```
+var log = cwlog.logger(6);
+```
+
+Usage.
+
+```
+log.trace('the message');
+log.fatal('a serious notice');
+
+log.trace(cwlog.t(), 'with a timestamp', 42, {key: 'multiple arguments'});
+```
+
+## API
+
+**static cwlog**
+- logger(level: number\[0-6\]): Logger instance
+- t(): string - The ISO timestamp
+
+**Logger instance**
+- trace(...args)
+- debug(...args)
+- info(...args)
+- warn(...args)
+- error(...args)
+- fatal(...args)
+
+## Logging level
+such as Log4J.
+
+- 0: OFF
+- 1: FATAL
+- 2: ERROR
+- 3: WARN
+- 4: INFO
+- 5: DEBUG
+- 6: TRACE
+
+## Author
+- [OKUNOKENTARO](https://github.com/armorik83) aka armorik83
+
+## License
+MIT
---
+++ src/log.ts
@@ -0,0 +1,53 @@
+/**
+ * cw-log
+ *
+ * @copyright © 2015 OKUNOKENTARO
+ * @since cw-log v 0.1.0 (Mar 5, 2015)
+ */
+'use strict';
+
+module cw {
+  export class Log {
+    constructor(private level: number) {
+      //
+    }
+
+    private logging(level: number): Function {
+      if (this.level === 0) {return () => {}}
+
+      if (level === 6 && level <= this.level) {return console.log  .bind(console)}
+      if (level === 5 && level <= this.level) {return console.log  .bind(console)}
+      if (level === 4 && level <= this.level) {return console.info .bind(console)}
+      if (level === 3 && level <= this.level) {return console.warn .bind(console)}
+      if (level === 2 && level <= this.level) {return console.error.bind(console)}
+      if (level === 1 && level <= this.level) {return console.error.bind(console, '[FATAL]')}
+      return () => {};
+    }
+
+    trace = this.logging(6);
+    debug = this.logging(5);
+    info  = this.logging(4);
+    warn  = this.logging(3);
+    error = this.logging(2);
+    fatal = this.logging(1);
+
+    static t() {
+      return new Date().toISOString() + ' |';
+    }
+
+    static logger(level: number) {
+      return new Log(level);
+    }
+  }
+}
+
+this.cwlog = cw.Log;
+
+declare var module;
+declare var define;
+
+if (typeof module === 'object' && module.exports) {
+  module.exports = cw.Log;
+} else if (typeof define === 'function' && define.amd) {
+  define(function(){return cw.Log});
+}
